84f843c656f2184b785cc807aee96787fb572ec8
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-05-12 00:12:59 +0700

    :zap: remove unused route

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var sign_in = require('./routes/sign_in.js')
-var index = require('./routes/index.js')
 var express = require('express')
 var app = express()
 var mongoose = require('mongoose')
@@ -26,7 +25,7 @@ db.once('open', function () {
 
 // use once and other time get all :D, thatz a trick
 app.use('/', sign_in)
-app.all('/index', index)
+//app.all('/index', index)
 
 var server = app.listen(8080, () =>{
     console.log("The server is now running at http://localhost:8080");
